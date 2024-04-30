beb341548896aeac7d2e91c24dd34f86da2135cd
AzawuntuBlatena <azabla1227@gmail.com>
2024-04-30 13:30:59 +0300

    Initial Author Route

--- server.js
+++ server.js
@@ -5,15 +5,19 @@ if(process.env.NODE_ENV !== 'production'){
 const express = require('express')
 const app = express()
 const expressLayouts = require('express-ejs-layouts')
+const bodyparser = require('body-parser')
 
 
 const indexRouter = require('./routes/index')
+const authorRouter = require('./routes/authors')
+
 
 app.set('view engine', 'ejs')
 app.set('views', __dirname+'/views')
 app.set('layout', 'layouts/layout')
 app.use(expressLayouts)
 app.use(express.static('public'))
+app.use(bodyparser.urlencoded({limit: '10mb', extended: true}))
 
 const mongoose = require('mongoose')
 mongoose.connect(process.env.DATABASE_URL, {useNewUrlParser: true})
@@ -24,5 +28,7 @@ db.once('open', () => console.log("connected to Mongoose"))
 
 
 app.use('/', indexRouter)
+app.use('/authors', authorRouter)
+
 
 app.listen(process.env.PORT || 3000)
